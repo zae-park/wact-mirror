e92aa692dc068177f00ff8ea4f3d69325c86e489
iceprins <junghankyul@gmail.com>
2024-04-13 21:11:57 +0900

    feat: add push initial notification receiver

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,17 +1,87 @@
+//import UIKit
+//import Flutter
+
+//@UIApplicationMain
+//@objc class AppDelegate: FlutterAppDelegate {
+//  override func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//  ) -> Bool {
+//
+//    if #available(iOS 10.0, *) {
+//      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+//    }
+//    GeneratedPluginRegistrant.register(with: self)
+//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//  }
+//}
+
 import UIKit
-import Flutter
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
 
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        registerForPushNotifications(application: application)
+        return true
     }
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print(error)
+    }
+
+
+    @available(iOS 13.0, *)
+    private func registerForPushNotifications(application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) {
+            (granted, error) in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+    }
+
+
+}
+
+extension AppDelegate: MessagingDelegate {
+  @available(iOS 13.0, *)
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
   }
 }
